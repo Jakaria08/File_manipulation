98812fee7e3242364d6201b243f343f39192e170
jakaria08 <jakaria.rabbi@live.com>
2020-01-12 17:44:52 -0700

    first commit

--- file_name_change.py
+++ file_name_change.py
@@ -1,22 +1,12 @@
 import os
 import glob
-import xml.etree.ElementTree as ET
 
-image_path = os.path.join(os.getcwd(), 'images')
-count = 0
+image_path = "/home/jakaria/Super_Resolution/CinCGAN-pytorch/datasets/SPOT-6-BING/DIV2K/DIV2K_train_HR"
+count = 1
 
-for xml_file in glob.glob(image_path + '/*.xml'):
-    tree = ET.parse(xml_file)
-    root = tree.getroot()
-    for b in root.findall('object'):
-        c = b.find('name')
-        if c.text.strip() == 'Field0':
-            c.text = 'empty_site'
-        elif c.text.strip() == 'Field1':
-            c.text = 'single_tank'
-        else:
-            c.text = 'multiple_tank'
+for img_file in glob.glob(image_path + '/*.png'):
+    new_name = str(count).zfill(4) + ".png"
+    print(new_name)
     count += 1
     if count%100 == 0:
         print(count)
-    tree.write(xml_file)
