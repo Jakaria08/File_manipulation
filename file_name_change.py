c662ee86ef6bbf226bb76193d154bcb41a72f97b
jakaria08 <jakaria.rabbi@live.com>
2020-01-12 18:57:28 -0700

    first commit

--- file_name_change.py
+++ file_name_change.py
@@ -6,7 +6,7 @@ image_path = "/home/jakaria/Super_Resolution/CinCGAN-pytorch/datasets/SPOT-6-BIN
 count = 1
 
 for img_file in glob.glob(image_path + '/*.png'):
-    dir_name = os.path.join(os.dirname(image_path), "DIV2K_train_LR_bicubic")
+    dir_name = os.path.join(os.dirname(image_path), "DIV2K_train_LR_bicubic/X4")
 
     img =  cv2.imread(img_file)
     resize_image = cv2.resize(img, (256,256), interpolation=cv2.INTER_CUBIC)
