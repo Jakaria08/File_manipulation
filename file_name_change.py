aaebeebe8fb39984d9b479390a1aeb27492542c0
jakaria08 <jakaria.rabbi@live.com>
2020-01-12 18:56:36 -0700

    first commit

--- file_name_change.py
+++ file_name_change.py
@@ -1,13 +1,19 @@
 import os
 import glob
+import cv2
 
-image_path = "/home/jakaria/Super_Resolution/CinCGAN-pytorch/datasets/SPOT-6-BING/DIV2K/DIV2K_valid_LR_mild"
-count = 801
+image_path = "/home/jakaria/Super_Resolution/CinCGAN-pytorch/datasets/SPOT-6-BING/DIV2K/DIV2K_train_HR"
+count = 1
 
 for img_file in glob.glob(image_path + '/*.png'):
-    dir_name = os.path.dirname(img_file)
-    new_name = os.path.join(dir_name, str(count).zfill(4) + "x4m.png")
-    os.rename(img_file,new_name)
+    dir_name = os.path.join(os.dirname(image_path), "DIV2K_train_LR_bicubic")
+
+    img =  cv2.imread(img_file)
+    resize_image = cv2.resize(img, (256,256), interpolation=cv2.INTER_CUBIC)
+
+    new_name = os.path.join(dir_name, str(count).zfill(4) + "x4.png")
+    cv.imwrite(new_name, resize_image)
+
     count += 1
     if count%100 == 0:
         print(count)
