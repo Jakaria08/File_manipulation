2fe36b91d1b076545c1b263af55e73c6fda92282
jakaria08 <jakaria.rabbi@live.com>
2020-01-12 19:02:01 -0700

    first commit

--- file_name_change.py
+++ file_name_change.py
@@ -2,11 +2,11 @@ import os
 import glob
 import cv2
 
-image_path = "/home/jakaria/Super_Resolution/CinCGAN-pytorch/datasets/SPOT-6-BING/DIV2K/DIV2K_train_HR"
-count = 1
+image_path = "/home/jakaria/Super_Resolution/CinCGAN-pytorch/datasets/SPOT-6-BING/DIV2K/DIV2K_valid_HR"
+count = 801
 
 for img_file in glob.glob(image_path + '/*.png'):
-    dir_name = os.path.join(os.path.dirname(image_path), "DIV2K_train_LR_bicubic/X4")
+    dir_name = os.path.join(os.path.dirname(image_path), "DIV2K_valid_LR_bicubic/X4")
 
     img =  cv2.imread(img_file)
     resize_image = cv2.resize(img, (256,256), interpolation=cv2.INTER_CUBIC)
