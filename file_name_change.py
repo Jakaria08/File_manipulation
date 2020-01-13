f9698a3934b27798b36a5caf957dcea38fe81764
jakaria08 <jakaria.rabbi@live.com>
2020-01-12 18:28:59 -0700

    first commit

--- file_name_change.py
+++ file_name_change.py
@@ -5,7 +5,7 @@ image_path = "/home/jakaria/Super_Resolution/CinCGAN-pytorch/datasets/SPOT-6-BIN
 count = 1
 
 for img_file in glob.glob(image_path + '/*.png'):
-    dir_name = os.path.dir_name(img_file)
+    dir_name = os.path.dirname(img_file)
     new_name = os.path.join(dir_name, str(count).zfill(4) + ".png")
     print(new_name)
     print(img_file)
